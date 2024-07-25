3ade768465d36d8714d91978734345c1cd8229d8
Dominik Ullrich <dominik119017@gmail.com>
2024-07-25 14:46:12 +0200

    add comments for all possible gnome excludes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,6 +72,28 @@
     gnome.geary
     gnome.epiphany
   ]; # Maps, Web, Geary, contacts, weather
+
+  # environment.gnome.excludePackages = with pkgs.gnome; [
+  #   baobab      # disk usage analyzer
+  #   cheese      # photo booth
+  #   eog         # image viewer
+  #   epiphany    # web browser
+  #   gedit       # text editor
+  #   simple-scan # document scanner
+  #   totem       # video player
+  #   yelp        # help viewer
+  #   evince      # document viewer
+  #   file-roller # archive manager
+  #   geary       # email client
+  #   seahorse    # password manager
+
+  #   # these should be self explanatory
+  #   gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-contacts
+  #   gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-photos gnome-screenshot
+  #   gnome-system-monitor gnome-weather gnome-disk-utility pkgs.gnome-connections
+  # ];
+
+
   # Configure keymap in X11
   services.xserver.xkb.layout = "de";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
