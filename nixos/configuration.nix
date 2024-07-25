fab6c891b9a01b1bd0c4c971a30f661ee66d36ef
Domi <dominik119017@gmail.com>
2024-07-25 12:00:21 +0200

    git vergessen

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -102,6 +102,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
     wget
     firefox
     tree
