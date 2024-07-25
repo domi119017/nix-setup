ada8ee7ad8276a87845c92384ae7f5696de75f14
Dominik Ullrich <dominik119017@gmail.com>
2024-07-25 12:25:03 +0200

    add p10k to home.packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,6 +35,7 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
     gnomeExtensions.user-themes
+    zsh-powerlevel10k
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
