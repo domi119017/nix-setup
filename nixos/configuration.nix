8a3c3043c8e7c997d8905721094881d5fa5fcac3
Dominik Ullrich <dominik119017@gmail.com>
2024-07-27 15:31:10 +0200

    pls sops

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,7 +8,19 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.sops-nix.nixosModules.sops
     ];
+
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+
+  sops.age.keyFile = /home/delta/.config/sops/age/keys.txt;
+
+  sops.secrets = {
+    ssh_key = {};
+    delta_passwd = {};
+    admin_passwd = {};
+  };
 
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
