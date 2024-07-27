4cfc0506052161c53ce8cc81a7d002c9413cfa70
Dominik Ullrich <dominik119017@gmail.com>
2024-07-27 13:47:47 +0200

    add jetbrains mono nf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -145,14 +145,14 @@
     gnome3.gnome-tweaks
     obsidian
   ];
+
+  fonts.packages = with pkgs; [
+    # Jet Brains Mono NF
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
   programs.zsh.enable = true;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+
 
   # List services that you want to enable:
 
