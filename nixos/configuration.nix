d6e270eddcd05d2ad47cdd0a19fe42f44c7f429c
Dominik Ullrich <dominik119017@gmail.com>
2024-07-27 17:02:18 +0200

    dgf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -214,10 +214,10 @@
           path = "/home/delta/Sync/Mindustry";
           devices = [ "hawking" ];
         };
-        gui = {
-          user = "$(cat ${config.sops.secrets.st_username.path})";
-          password = "$(cat ${config.sops.secrets.st_passwd.path})";
-        };
+      };
+      gui = {
+        user = "$(cat ${config.sops.secrets.st_username.path})";
+        password = "$(cat ${config.sops.secrets.st_passwd.path})";
       };
     };
   };
