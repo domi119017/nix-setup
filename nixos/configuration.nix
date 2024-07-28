d9abeaf92230ee6bd7c59dab8543d4b845defb1a
Domi <dominik119017@gmail.com>
2024-07-28 13:45:35 +0200

    htop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,6 +140,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
+    htop
     # firefox
     tree
     home-manager
