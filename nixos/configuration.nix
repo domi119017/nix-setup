dee18af299091800d5ed1f84cca6fe252ffa68bc
Domi <dominik119017@gmail.com>
2024-07-28 22:45:59 +0200

    add bat

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -138,6 +138,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bat
     git
     wget
     htop
