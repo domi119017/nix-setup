c701b731ab9ea5fea12069f349c1b984510347ce
Dominik Ullrich <dominik119017@gmail.com>
2024-07-29 13:40:29 +0200

    add nh (nix helper)

--- modules/common.nix
+++ modules/common.nix
@@ -53,4 +53,10 @@
   ];
 
   programs.zsh.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/delta/nix-setup";
+  };
 }
