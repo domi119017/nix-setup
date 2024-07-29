6f87422b358afe542c7e5e6274ac7b6b5935cc36
Dominik Ullrich <dominik119017@gmail.com>
2024-07-29 14:02:55 +0200

    try pipewire

--- modules/common.nix
+++ modules/common.nix
@@ -36,10 +36,10 @@
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  #  };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     # --- Actually required --- #
