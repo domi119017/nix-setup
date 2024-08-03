2d31d5dd7724d73c498eb303dac3ef2a91524a6d
Domi <dominik119017@gmail.com>
2024-08-03 12:37:35 +0200

    sops

--- nixos/hosts/surfacego2/default.nix
+++ nixos/hosts/surfacego2/default.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # inputs.sops-nix.nixosModules.sops
       ../../modules/common.nix
       # ../../modules/coding.nix
       ../../modules/gnome.nix
@@ -18,22 +17,22 @@
       # ../../modules/notes.nix
 
       # Sops
-      inputs.sops-nix.nixosModules.sops
+      # inputs.sops-nix.nixosModules.sops
     ];
 
-  sops.defaultSopsFile = ../../../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
+  # sops.defaultSopsFile = ../../../secrets/secrets.yaml;
+  # sops.defaultSopsFormat = "yaml";
 # 
-  sops.age.keyFile = "/home/delta/.config/sops/age/keys.txt";
+  # sops.age.keyFile = "/home/delta/.config/sops/age/keys.txt";
 # 
-  sops.secrets = {
-    ssh_key = {};
-    delta_passwd = {};
-    admin_passwd = {};
-    hawking_st_id = {};
-    st_passwd = {};
-    st_username = {};
-  };
+  # sops.secrets = {
+  #   ssh_key = {};
+  #   delta_passwd = {};
+  #   admin_passwd = {};
+  #   hawking_st_id = {};
+  #   st_passwd = {};
+  #   st_username = {};
+  # };
 # 
   # sops.templates = {
     # "ssh_key".content = ''${config.sops.placeholder.ssh_key}'';
